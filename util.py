542cdfa9e13f18b61852d3e88822bd65bb7cb4b4
Mac Yang <mac.yang@gmail.com>
2015-03-13 21:27:35 +0000

    #3 added codeblock_toggle() to hide/show the code blocks

--- util.py
+++ util.py
@@ -2,6 +2,25 @@ import json
 import sys
 import numpy as np
 
+from IPython.display import HTML
+
+def codeblock_toggle():
+  return HTML('''
+    <script>
+      code_show=true; 
+      function code_toggle() {
+       if (code_show){
+       $('div.input').hide();
+       } else {
+       $('div.input').show();
+       }
+       code_show = !code_show
+      } 
+      $( document ).ready(code_toggle);
+    </script>
+    <form action="javascript:code_toggle()"><input type="submit" value="Toggle on/off the code blocks."></form>
+    ''')
+
 def createNumpyArray(data):
   output = []
   for dS in data:
